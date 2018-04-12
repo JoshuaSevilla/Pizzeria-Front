16471a97cbe5c177818fd7e0207dbf79ecddf4c0
Joshua Sevilla Alexander <37308758+JoshuaSevilla@users.noreply.github.com>
2018-04-12 14:02:58 +0200

    Added confirm password

--- Pizzeria-Front-Angularjs/src/modules/user/services/userservice.js
+++ Pizzeria-Front-Angularjs/src/modules/user/services/userservice.js
@@ -3,7 +3,8 @@ import { BaseService } from '../../baseservice';
 
 export class UserService extends BaseService {
     constructor(http, resolveUrl) {
-        super(http, resolveUrl, 'api/Account/Register');
+        super(http, resolveUrl, 'api/Account/Register', 'Token');
+
     }
 
     get(id) {
@@ -11,60 +12,28 @@ export class UserService extends BaseService {
             super.getRouteById(id)
         );
     }
+    
     getAll() {
         return this.http.get(
             super.getRoute()
         );
     }
+
     create(user) {
-        // in the development database (json-server)
-        // we do not store the images
-        if(user.img) delete user.img;
         return this.http.post(
             super.getRoute(),
             user
         );
-
-        $http({
-            method: 'POST',
-            url: super.getRoute(),
-            //headers: { 'Content-Type': "application/x-www-form-urlencoded" },
-            data: JSON.stringify(this.user)
-        }).then(function successCallback(response) {
-            console.log('success');
-            respuesta = JSON.parse(response);
-        }, function errorCallback(response) {
-            console.log('error');
-            console.log(response);
-        });
-    
-
-        /*
-        return this.http({
-            method: 'POST',
-            // set content-type to undefined so it is automatically selected
-            headers: { 'Content-Type': undefined },
-            url: super.getRoute(),
-            data: {
-                name: pizza.name,
-                ingredients: pizza.ingredients,
-                image: pizza.img,
-            },
-            transformRequest: function (data, headersGetter) {
-                let formData = new FormData();
-                angular.forEach(data, function (value, key) {
-                    if (key === 'image' && value) {
-                        formData.append(key, value, value.name);
-                    }
-                    else formData.append(key, value);
-                });
-                return formData;
-            }
-        });
-        */
     }
-   
-    
+
+    login(user) {
+        var data = "grant_type=password&username=" + user.email + "&password=" + user.password;
+        return this.http.post(
+            super.getRouteToken(),
+            data,
+            { headers: { 'Content-Type': "application/x-www-form-urlencoded" } },
+        );
+    }
 }
 UserService.$inject = ['$http', 'resolveUrl'];
 App.service('userService', UserService);
